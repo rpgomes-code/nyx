6b8887899ad6054882dc1489b77677d649b2f5de
Rui Gomes <gpsprui@gmail.com>
2025-06-22 16:43:44 +0100

    Add authentication setup, theme provider, and Prisma client integration

---
+++ src/lib/auth-client.ts
@@ -0,0 +1,4 @@
+﻿import { createAuthClient } from "better-auth/react"
+export const authClient = createAuthClient({
+    baseURL: process.env.BASE_URL,
+})
---
+++ src/lib/auth.ts
@@ -0,0 +1,33 @@
+﻿import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "@/generated/prisma";
+import {nextCookies} from "better-auth/next-js";
+
+const prisma = new PrismaClient();
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "sqlite",
+    }),
+    emailAndPassword: {
+        enabled: true
+    },
+    socialProviders: {
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        },
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+        apple: {
+            clientId: process.env.APPLE_CLIENT_ID as string,
+            clientSecret: process.env.APPLE_CLIENT_SECRET as string,
+        },
+        discord: {
+            clientId: process.env.DISCORD_CLIENT_ID as string,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+        }
+    },
+    plugins: [nextCookies()]
+});
